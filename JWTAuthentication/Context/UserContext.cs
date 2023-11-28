dd3c3836bc395ac9904036f034bf22494b6b7f0c
gizemyildiz22 <ugurlucelikgizem@gmail.com>
2023-11-28 20:53:13 +0300

    deneme-gizemyıldız

--- JWTAuthentication/Context/UserContext.cs
+++ JWTAuthentication/Context/UserContext.cs
@@ -6,7 +6,7 @@ namespace JWTAuthentication.Context
 {
     public class UserContext : DbContext
     {
-        public DbSet<UserDMO> Users { get; set; }
+        public DbSet<UserDMO> Users { get; set;}
         public UserContext(DbContextOptions option) : base(option)
         {
 
