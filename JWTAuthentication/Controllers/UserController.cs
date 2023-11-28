fb2bff1c24eeb03ec2da821cd92622784594f376
Ahmet Furkan Gümüş <afurkan.gms@outlook.com>
2023-11-28 20:53:25 +0300

    furkan-değişken tanımlandı

--- JWTAuthentication/Controllers/UserController.cs
+++ JWTAuthentication/Controllers/UserController.cs
@@ -68,7 +68,7 @@ namespace JWTAuthentication.Controllers
         public IActionResult UserDetail()
         {
 
-            string furkan = "Gumus";
+            var b = 120;
 
             return Ok(new { userid = 1 });
 
